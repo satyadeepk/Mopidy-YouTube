8c6f5e02f9e7c7b64740a659120ad2f981a8f1dd
Satyadeep <satyadeep.1991@gmail.com>
2016-12-28 14:37:01 +0530

    Fixing download for youtube ids start with hyphen

--- mopidy_youtube/downloader.py
+++ mopidy_youtube/downloader.py
@@ -3,20 +3,21 @@ import logging
 import youtube_dl
 
 def download_if_not_exists(download_dir, youtubeId):
-    filepath = download_dir + youtubeId + '.mp3'
+    filepath = download_dir + 'yt_' + youtubeId + '.mp3'
     if not os.path.isfile(filepath):
         command_tokens = [
             'youtube-dl',
             '--extract-audio',
             '--audio-format mp3',
             '--audio-quality 5',
-            '--output \'' +  download_dir + '%(id)s.%(ext)s\'',
+            '--output \'' +  download_dir + 'yt_%(id)s.%(ext)s\'',
             # '--output \'~/Music/%(title)s.%(ext)s\'',
             youtubeId]
         
         command = ' '.join(command_tokens)
         print('Downloading ' + youtubeId)
         os.system(command)
+        print('Download ' + youtubeId + ' complete.')
     return filepath
 
 def download_uris(download_dir, trackuris):
